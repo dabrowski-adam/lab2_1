ed5e993aeb59a03a524751fc9112ebd10eef54fc
Adam Dąbrowski <190139@edu.p.lodz.pl>
2019-03-21 17:51:49 +0100

    testy_v1 Add test for failure and length 1

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -16,4 +16,15 @@ public class BinarySearchTest {
         assertTrue(searchResult.isFound());
         assertEquals(key, seq[searchResult.getPosition()]);
     }
+
+    @Test
+    public void searchFailsInLength1() {
+        int[] seq = { 0 };
+        int key = 1;
+
+        SearchResult searchResult = BinarySearch.search(key, seq);
+
+        assertFalse(searchResult.isFound());
+        assertEquals(-1, searchResult.getPosition());
+    }
 }
